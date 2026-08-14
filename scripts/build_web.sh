dba6960a462abb4e283f0186e895cafa3a115d7c
Gytjftjygt <92408804+apkile78@users.noreply.github.com>
2026-08-14 07:22:02 -0400

    Patch build_web.sh to modify EMCC_CFLAGS
    Updated build-emscripten.sh to include additional EMCC_CFLAGS for unused function warnings.

--- scripts/build_web.sh
+++ scripts/build_web.sh
@@ -145,7 +145,7 @@ export LDFLAGS="$LDFLAGS --use-port=sdl3 --use-port=sdl3_ttf /tmp/sdl3_image_pre
 sed -i 's/ifeq (\$(SDL3_DO_VERSION_CHECK),1)/ifeq ($(SDL3_DO_VERSION_CHECK),SKIP)/' Makefile
 
 echo "Patching build-emscripten.sh to allow the experimental SDL3 port..."  
-sed -i 's#^\(\s*\)make #\1EMCC_CFLAGS="--use-port=sdl3 --use-port=sdl3_ttf -Wno-experimental -Wno-unused-template -I/tmp/sdl3_image_prefix/include -DUSE_SDL3" make PCH=0 #' build-scripts/build-emscripten.sh
+sed -i 's#^\(\s*\)make #\1EMCC_CFLAGS="--use-port=sdl3 --use-port=sdl3_ttf -Wno-experimental -Wno-unused-template -Wno-unused-function -I/tmp/sdl3_image_prefix/include -DUSE_SDL3" make PCH=0 #' build-scripts/build-emscripten.sh
 
 # ---- Option A: build SDL3_image from source to a wasm static lib ----  
 SDL_PREFIX="/tmp/sdl3_image_prefix"  
