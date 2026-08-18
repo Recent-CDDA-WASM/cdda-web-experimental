729303fb6699777d3af02d76048476db5207e5df
Gytjftjygt <92408804+apkile78@users.noreply.github.com>
2026-08-18 12:46:29 -0400

    Enhance wasm-opt command with additional flags
    Updated wasm-opt command to enable bulk memory and threads.

--- scripts/build_web.sh
+++ scripts/build_web.sh
@@ -228,8 +228,8 @@ echo "Using wasm-opt at: $WASM_OPT"
   
 echo "wasm size BEFORE wasm-opt:"  
 ls -lh "$OUTPUT_ABS_PATH/cataclysm-tiles.wasm"  
-  
-"$WASM_OPT" -Oz "$OUTPUT_ABS_PATH/cataclysm-tiles.wasm" -o "$OUTPUT_ABS_PATH/cataclysm-tiles.wasm.opt"  
+
+"$WASM_OPT" -Oz --enable-bulk-memory --enable-threads "$OUTPUT_ABS_PATH/cataclysm-tiles.wasm" -o "$OUTPUT_ABS_PATH/cataclysm-tiles.wasm.opt"
 mv "$OUTPUT_ABS_PATH/cataclysm-tiles.wasm.opt" "$OUTPUT_ABS_PATH/cataclysm-tiles.wasm"
   
 echo "wasm size AFTER wasm-opt:"  
